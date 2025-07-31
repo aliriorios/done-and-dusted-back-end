83cc1eca2c1c53abd4461cdc288b97506abea0ce
Alírio Rios <alirioriosmc@gmail.com>
2025-07-31 12:03:56 -0300

    Realizando testes unitarios de User Service (find)

--- src/test/java/br/com/aliriorios/done_and_dusted/ut/service/UserServiceUnitTest.java
+++ src/test/java/br/com/aliriorios/done_and_dusted/ut/service/UserServiceUnitTest.java
@@ -1,6 +1,7 @@
 package br.com.aliriorios.done_and_dusted.ut.service;
 
 import br.com.aliriorios.done_and_dusted.entity.User;
+import br.com.aliriorios.done_and_dusted.exception.EntityNotFoundException;
 import br.com.aliriorios.done_and_dusted.exception.UsernameUniqueViolationException;
 import br.com.aliriorios.done_and_dusted.repository.UserRepository;
 import br.com.aliriorios.done_and_dusted.service.UserService;
@@ -14,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -71,5 +74,71 @@ public class UserServiceUnitTest {
 
         verify(passwordEncoder, times(1)).encode(anyString());
         verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("Successfully test when everything is OK")
+    void findById_Successfully() {
+        User user = UserMapper.toUser(new RegisterDto("user.test@email.com", "123456", "User Test"));
+        user.setId(1L);
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(user));
+
+        User responseBody = userService.findById(user.getId());
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getId()).isNotNull();
+        assertThat(responseBody.getId()).isEqualTo(1L);
+        assertThat(responseBody.getUsername()).isEqualTo("user.test@email.com");
+        verify(userRepository, times(1)).findById(any(Long.class));
+    }
+
+    @Test
+    @DisplayName("Test failed, throw exception when the user not found")
+    void findById_Failed() {
+        User user = UserMapper.toUser(new RegisterDto("user.test@email.com", "123456", "User Test"));
+        user.setId(1L);
+
+        when(userRepository.findById(any(Long.class)))
+                .thenThrow(new EntityNotFoundException(String.format("User [id=%s] not founded.", user.getId())));
+
+        assertThatThrownBy(() -> userService.findById(user.getId()))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessageContaining("User [id=1] not founded.");
+
+        verify(userRepository, times(1)).findById(any(Long.class));
+    }
+
+    @Test
+    @DisplayName("Successfully test when everything is OK")
+    void findByUsername_Successfully() {
+        User user = UserMapper.toUser(new RegisterDto("user.test@email.com", "123456", "User Test"));
+        user.setId(1L);
+
+        when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.of(user));
+
+        User responseBody = userService.findByUsername(user.getUsername());
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getId()).isNotNull();
+        assertThat(responseBody.getId()).isEqualTo(1L);
+        assertThat(responseBody.getUsername()).isEqualTo("user.test@email.com");
+        verify(userRepository, times(1)).findByUsername(any(String.class));
+    }
+
+    @Test
+    @DisplayName("Test failed, throw exception when the user not found")
+    void findByUsername_Failed() {
+        User user = UserMapper.toUser(new RegisterDto("user.test@email.com", "123456", "User Test"));
+        user.setId(1L);
+
+        when(userRepository.findByUsername(any(String.class)))
+                .thenThrow(new EntityNotFoundException(String.format("User [%s] not founded", user.getUsername())));
+
+        assertThatThrownBy(() -> userService.findByUsername(user.getUsername()))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessageContaining("User [user.test@email.com] not founded");
+
+        verify(userRepository, times(1)).findByUsername(any(String.class));
     }
 }
